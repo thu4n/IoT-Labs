52375e012f122568fad34f89ae830798804c24da
r1anl3 <levu1409@gmail.com>
2024-03-11 14:59:45 +0700

    rolling dice

--- Lab01/ex03/ex03.ino
+++ Lab01/ex03/ex03.ino
@@ -1,5 +1,3 @@
-#include <random>
-
 const int ledPins[6] = {2, 3, 4, 5, 6, 7};
 const int buttonPin = 8;
 
@@ -14,12 +12,23 @@ void loop() {
   if (digitalRead(buttonPin) == LOW) {
     int randomNumber = random(1, 7);
     for (int i = 0; i < 6; i++) {
-      digitalWrite(ledPins[i], (i + 1) <= randomNumber ? HIGH : LOW);
+      digitalWrite(ledPins[i], HIGH);
+      delay(100);
     }
     for (int i = 0; i < 6; i++) {
-      digitalWrite(ledPins[i], HIGH);
-      delay(50);
-      digitalWrite(ledPins[i], LOW);
+      digitalWrite(ledPins[i], (i + 1) <= randomNumber ? HIGH : LOW);
+    }
+    delay(1000);
+    for (int i = 0; i < 6; i++)
+    {
+      for (int i = 0; i < 6; i++) {
+        digitalWrite(ledPins[i], HIGH);
+      }
+      delay(100);
+      for (int i = 0; i < 6; i++) {
+        digitalWrite(ledPins[i], LOW);
+      }
+      delay(100);
     }
     delay(1000);
   }
