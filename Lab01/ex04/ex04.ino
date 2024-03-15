1aa02e3228844b710b9542f6fd591d0106699080
r1anl3 <levu1409@gmail.com>
2024-03-15 14:07:46 +0700

    fix number

--- Lab01/ex04/ex04.ino
+++ Lab01/ex04/ex04.ino
@@ -1,12 +1,12 @@
 // Kịch bản: giao lộ B bên trái, giao lộ A bên phải
 // Khai báo các chân led 7 đoạn
-unsigned const int A = 13;
-unsigned const int B = 12;
-unsigned const int C = 11;
-unsigned const int D = 10;
+unsigned const int A = 11;
+unsigned const int B = 10;
+unsigned const int C = 7;
+unsigned const int D = 8;
 unsigned const int E = 9;
-unsigned const int F = 8;
-unsigned const int G = 7;
+unsigned const int F = 12;
+unsigned const int G = 13;
 unsigned const int H = 6;
 // Khai báo các chân led đèn giao thông
 unsigned const int red_A = 5;
@@ -20,96 +20,86 @@ bool turn = 0; // 0: B, 1: A
 
 void zero(void) {
   digitalWrite(A, LOW);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
   digitalWrite(G, HIGH);
   digitalWrite(H, LOW);
 }
 
 void one(void) {
-  digitalWrite(A, LOW);
+  digitalWrite(A, HIGH);
   digitalWrite(B, LOW);
   digitalWrite(C, LOW);
   digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
   digitalWrite(G, HIGH);
-  digitalWrite(H, LOW);
+  digitalWrite(H, HIGH);
 }
 
 void two(void) {
-  digitalWrite(A, HIGH);
+  digitalWrite(A, LOW);
   digitalWrite(B, LOW);
   digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
+  digitalWrite(D, LOW);
+  digitalWrite(E, LOW);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, LOW);
+  digitalWrite(H, HIGH);
+}
+
+void three(void) {
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
   digitalWrite(E, HIGH);
   digitalWrite(F, HIGH);
   digitalWrite(G, LOW);
   digitalWrite(H, LOW);
-}
-
-void three(void) {
-  digitalWrite(A, HIGH);
-  digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
-  digitalWrite(H, LOW);
+  
 }
 
 void four(void) {
   digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
+  digitalWrite(B, LOW);
   digitalWrite(C, LOW);
   digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
+  digitalWrite(E, HIGH);
   digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
+  digitalWrite(G, LOW);
   digitalWrite(H, LOW);
 }
 
 void five(void) {
-  digitalWrite(A, HIGH);
+  digitalWrite(A, LOW);
   digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
+  digitalWrite(C, LOW);
   digitalWrite(D, LOW);
-  digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
   digitalWrite(H, LOW);
 }
 
 void six(void) {
-  digitalWrite(A, HIGH);
+  digitalWrite(A, LOW);
   digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
+  digitalWrite(C, LOW);
   digitalWrite(D, LOW);
-  digitalWrite(E, HIGH);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
+  digitalWrite(E, LOW);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
   digitalWrite(H, LOW);
 }
 
 void seven(void) {
   digitalWrite(A, LOW);
   digitalWrite(B, LOW);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
-  digitalWrite(E, LOW);
-  digitalWrite(F, LOW);
-  digitalWrite(G, HIGH);
-  digitalWrite(H, LOW);
-}
-
-void eight(void) {
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
+  digitalWrite(C, LOW);
   digitalWrite(D, HIGH);
   digitalWrite(E, HIGH);
   digitalWrite(F, HIGH);
@@ -117,14 +107,25 @@ void eight(void) {
   digitalWrite(H, LOW);
 }
 
-void nine(void) {
-  digitalWrite(A, HIGH);
-  digitalWrite(B, HIGH);
-  digitalWrite(C, HIGH);
-  digitalWrite(D, HIGH);
+void eight(void) {
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
   digitalWrite(E, LOW);
-  digitalWrite(F, HIGH);
-  digitalWrite(G, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
+  digitalWrite(H, LOW);
+}
+
+void nine(void) {
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, LOW);
+  digitalWrite(G, LOW);
   digitalWrite(H, LOW);
 }
 
