0b3fcd5add61516c424aba7951bdac7b17eed213
r1anl3 <levu1409@gmail.com>
2024-05-07 08:53:30 +0700

    add lab04

---
+++ Lab04/Lab04.ino
@@ -0,0 +1,135 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
+#include <DHT22.h>
+#include <ArduinoJson.h>
+
+//define pin data
+#define pinDATA D4
+#define LED_PIN D3
+
+DHT22 dht22(pinDATA); 
+
+const char* ssid = "UIT Public";
+const char* password = "";
+const char* mqtt_server = "mqtt.thingsboard.cloud";
+const char* topic = "v1/devices/me/telemetry";
+const String username = "1n2DNxT7sDDA0ycBziSe"
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+float temp = 0;
+float hum = 0;
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  if ((char)payload[0] == '1') {
+    blinkLed();
+  } 
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    // String clientId = "ESP8266Client-";
+    // clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(username.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void readDHT22() {
+  temp = dht22.getTemperature();
+  hum = dht22.getHumidity();
+}
+
+void blinkLed() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);
+}
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  readDHT22();
+  JsonDocument doc;
+  doc["temperature"] = String(temp);
+  doc["humidity"] = String(hum);
+
+  char jsonString[128];
+  serializeJson(doc, jsonString);
+
+  Serial.println(jsonString);
+  client.publish(topic, jsonString);
+  Serial.println("Published to: " + String(topic));
+  delay(2000);
+
+  client.loop();
+}
