3b6a410cb800648cd44132bd294fe37930ea8e8c
thu4n <thuan.tongg@gmail.com>
2024-06-02 15:06:04 +0700

    small change

--- Lab05/src/align_dataset_mtcnn.py
+++ Lab05/src/align_dataset_mtcnn.py
@@ -49,7 +49,6 @@ def main(args):
     print('Creating networks and loading parameters')
     
     with tf.Graph().as_default():
-        #gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=args.gpu_memory_fraction)
         sess = tf.compat.v1.Session()#config=tf.ConfigProto())#gpu_options=gpu_options, log_device_placement=False))
         with sess.as_default():
             pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
